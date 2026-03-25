c582993c16e228a85dd2312aec944b9e4fbf3068
nekogrile <nekogrile@github.com>
2026-03-25 15:46:36 +0100

    maj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, recall_score
 from io import StringIO
 
@@ -34,7 +34,7 @@ y = data["urgent"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = LogisticRegression(max_iter=1000)
+model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
